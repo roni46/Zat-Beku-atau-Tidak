ee95a960393c346ae4304efa2a5a9dce7052ac55
roni46 <39713502+roni46@users.noreply.github.com>
2019-01-03 17:58:46 +0700

    Add files via upload

---
+++ tugas program mengetahui bentuk bendak beku atau tidak.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    printf("PROOGRAM MENGHITUNG JARAK TEMPUH\n");
+    printf("BY M Achmad Sahroni NIM 311620782\n");
+    printf("KAMPUS PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+	int air;
+	cout<<"masukan derajat suhu=";
+	cin>>air;
+	if (air<0)
+	cout<<"air apabila terkena"<<"suhu"<<"air"<<"derajat adalah zat beku";
+	else if(air>0)
+	cout<<"air apabila terkena"<<"suhu"<<air<<"derajat adalah tidak zat beku";
+}
